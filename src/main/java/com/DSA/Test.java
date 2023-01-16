d920999f983b84d2f680fae357e3b99bddd2d19a
meansh <anshdotcontact@gmail.com>
2023-01-16 23:23:23 +0530

    Lambda expression

--- src/main/java/com/DSA/Test.java
+++ src/main/java/com/DSA/Test.java
@@ -4,16 +4,16 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 
-class Sorting implements Comparator<Integer>{
+/*class Sorting implements Comparator<Integer>{
 		public int compare(Integer i1, Integer i2) {
 			return (i1<i2)?-1:(i1>i2)?1:0;
-			/*
+			
 			 * if(i1<i2) return -1; else if(i1>i2) return 1; else return 0;
-			 */
+			 
 		}
-	}
+	}*/
 
-	public class Test {
+public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		ArrayList<Integer> l = new ArrayList<Integer>();
@@ -22,8 +22,11 @@ class Sorting implements Comparator<Integer>{
 		l.add(5);
 		l.add(8);
 		l.add(40);
-		Collections.sort(l, new Sorting());
+		//Lambda
+		Comparator<Integer> c = (i1, i2) -> (i1 < i2) ? -1 : (i1 > i2) ? 1 : 0;
+		// Collections.sort(l, new Sorting());
+		Collections.sort(l, c);
 		System.out.println(l);
 	}
-	
+
 }
